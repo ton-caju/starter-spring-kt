08b44c1d74a8ab26b48420e8cfd1b411dea1ba49
ton <alairton.dendena@caju.com.br>
2025-12-07 20:51:50 -0300

    fix: Added EntityScan

--- driven/persistence/src/main/kotlin/br/com/caju/driven/persistence/config/PersistenceConfig.kt
+++ driven/persistence/src/main/kotlin/br/com/caju/driven/persistence/config/PersistenceConfig.kt
@@ -1,5 +1,6 @@
 package br.com.caju.driven.persistence.config
 
+import org.springframework.boot.persistence.autoconfigure.EntityScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
@@ -7,4 +8,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @Configuration
 @EnableJpaRepositories(basePackages = ["br.com.caju.driven.persistence.repository"])
 @ComponentScan(basePackages = ["br.com.caju.driven.persistence"])
+@EntityScan(basePackages = ["br.com.caju.driven.persistence"])
 class PersistenceConfig
